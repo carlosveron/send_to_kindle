38ceb998cd2ad00146c91ec1a1b21cdc313c68fc
Carlos Veron <carloseduardo644@gmail.com>
2023-10-31 14:48:35 -0300

    modular added the main tree

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:send_to_kindle/app_modular.dart';
 import 'package:send_to_kindle/app_widget.dart';
+import 'package:send_to_kindle/shared/services/realm/realm_service.dart';
 
+final realmService = Modular.get<RealmService>();
 void main() {
-  runApp(const ProviderScope(child: MyApp()));
+  runApp(ProviderScope(
+      child: ModularApp(module: AppModule(), child: const MyApp())));
 }
