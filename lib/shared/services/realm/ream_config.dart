b69178f582ddffdd5d6d1ac5c256dbb33f73ca87
Carlos Veron <carloseduardo644@gmail.com>
2023-10-31 14:48:58 -0300

    added realm_config

---
+++ lib/shared/services/realm/ream_config.dart
@@ -0,0 +1,20 @@
+// var config = Configuration.local(
+//   [
+//     AudioPlayedModel.schema,
+//     DirectoryModel.schema,
+//     FileModel.schema,
+//     BookMarksModel.schema,
+//     MetaDataRealmModel.schema,
+//     SettingsModel.schema,
+//     ListeningLogModel.schema
+//   ],
+// );
+
+import 'package:realm/realm.dart';
+import 'package:send_to_kindle/shared/services/realm/models/books.dart';
+import 'package:send_to_kindle/shared/services/realm/models/user_settings.dart';
+
+var config = Configuration.local([
+  Books.schema,
+  UserSettings.schema,
+]);
