ea64c71a4b0c7982109a32f74c870773528c84da
Carlos Veron <carloseduardo644@gmail.com>
2023-11-09 14:17:04 -0300

    open browser to get kindle email

--- lib/pages/kindlemail_page/kindlemail_page.dart
+++ lib/pages/kindlemail_page/kindlemail_page.dart
@@ -5,6 +5,7 @@ import 'package:send_to_kindle/main.dart';
 import 'package:send_to_kindle/pages/home_page/my_home_page.dart';
 import 'package:send_to_kindle/shared/services/database/models/user_settings.dart';
 import 'package:send_to_kindle/shared/utils/utils.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class KindleEmailPage extends ConsumerStatefulWidget {
   const KindleEmailPage({super.key});
@@ -19,10 +20,8 @@ class _KindleEmailPageState extends ConsumerState<KindleEmailPage> {
 
   bool _isEmailValid = false;
   bool _isError = false;
-  @override
-  void initState() {
-    super.initState();
-  }
+  final url =
+      Uri.parse('https://www.amazon.com.br/hz/mycd/digital-console/alldevices');
 
   @override
   Widget build(BuildContext context) {
@@ -74,7 +73,25 @@ class _KindleEmailPageState extends ConsumerState<KindleEmailPage> {
                   });
                 },
                 child: const Text('Save'),
-              )
+              ),
+              const SizedBox(height: 10),
+              InkWell(
+                onTap: () async {
+                  // Add the action you want to perform when the link is clicked
+                  if (await canLaunchUrl(url)) {
+                    await launchUrl(url);
+                  } else {
+                    throw 'Could not launch ${url.host}';
+                  }
+                },
+                child: const Text(
+                  'Kindle email',
+                  style: TextStyle(
+                    color: Colors.blue,
+                    decoration: TextDecoration.underline,
+                  ),
+                ),
+              ),
             ],
           ),
         ));
