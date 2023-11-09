9a3cb3f9e6f149a7bc3bc1ac8443e55167caa845
Carlos Veron <carloseduardo644@gmail.com>
2023-11-09 14:37:42 -0300

    Remove unused comments

--- lib/shared/services/database/database_config.dart
+++ lib/shared/services/database/database_config.dart
@@ -1,15 +1,3 @@
-// var config = Configuration.local(
-//   [
-//     AudioPlayedModel.schema,
-//     DirectoryModel.schema,
-//     FileModel.schema,
-//     BookMarksModel.schema,
-//     MetaDataRealmModel.schema,
-//     SettingsModel.schema,
-//     ListeningLogModel.schema
-//   ],
-// );
-
 import 'package:realm/realm.dart';
 import 'package:send_to_kindle/shared/services/database/models/books.dart';
 import 'package:send_to_kindle/shared/services/database/models/user_settings.dart';
