8c4095733627c95c706a184b5e6eaa899ee5c7ce
Carlos Veron <carloseduardo644@gmail.com>
2023-11-09 12:25:25 -0300

    Added theme change

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:send_to_kindle/pages/splash_page/spash_page.dart';
+import 'package:send_to_kindle/shared/services/provider/providers.dart';
 import 'package:send_to_kindle/shared/theme/themes/themes.dart';
+import 'package:send_to_kindle/shared/utils/utils.dart';
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final userSettings = ref.watch(userSettingsProvider);
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Send to Kindle',
         theme: lightTheme,
         darkTheme: darkTheme,
+        themeMode: Utils.themeMode(userSettings?.isDarkThemeEnabled ?? true),
         home: const SplashPage());
   }
 }
